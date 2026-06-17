3086259d386d41ecda9fdd431e480fe9e645b7d0
coder149 <75641229+coder149@users.noreply.github.com>
2026-06-16 22:41:47 -0700

    feat: AnimatePresence route transitions in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
 import Landing from './pages/Landing';
 import Explore from './pages/Explore';
 import Destination from './pages/Destination';
 import Discoveries from './pages/Discoveries';
 import About from './pages/About';
 
-export default function App() {
+function AppRoutes() {
+  const location = useLocation();
   return (
-    <BrowserRouter>
-      <Routes>
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
         <Route path="/" element={<Landing />} />
         <Route path="/explore" element={<Explore />} />
         <Route path="/destination/:id" element={<Destination />} />
         <Route path="/discoveries" element={<Discoveries />} />
         <Route path="/about" element={<About />} />
       </Routes>
+    </AnimatePresence>
+  );
+}
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <AppRoutes />
     </BrowserRouter>
   );
 }
